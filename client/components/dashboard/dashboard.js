492accb6e09952a3ac4b967c35d05e033fe936a1
zainmustafa <zmdhillo@gmail.com>
2017-03-05 10:08:24 +0500

    dashboard updated!

--- client/components/dashboard/dashboard.js
+++ client/components/dashboard/dashboard.js
@@ -12,7 +12,7 @@
 (function () {
 
     angular.module("myApp")
-        .controller("DashboardController", ['$state', '$timeout', '$mdSidenav'  ,DashboardController]);
+        .controller("DashboardController", ['$state', '$timeout', '$mdSidenav' ,DashboardController]);
 
     function DashboardController($state, $timeout, $mdSidenav) {
         var vm = this;
@@ -27,15 +27,25 @@
         this.productName = "";
         this.productQuantity = 0;
         this.dateToDeliver = 0;
+        this.customerOrders = [{}];
+
         this.user = firebase.auth().currentUser.uid;
+        var orderRef = firebase.database().ref('order/' + vm.user + '/');
+        console.log("USER"+this.user);
 
         function buildToggler(componentId) {
             return function() {
                 $mdSidenav(componentId).toggle();
             };
+        }
+
+        this.incrementCustomerOrder = function () {
+            vm.customerOrders.push({
+                productName: vm.productName,
+                productQuantity: vm.productQuantity,
+                dateToDeliver: vm.dateToDeliver
+            });
         };
-
-
 
         this.logout = function () {
             firebase.auth().signOut().then(function() {
@@ -59,13 +69,22 @@
 
         this.placeOrder = function() {
             console.log("place order");
+            console.log(vm.customerOrders);
+            for(var i = 0; i < vm.customerOrders.length; i++)
+                firebase.database().ref('orders/' + vm.user+'/').push(vm.customerOrders[i]);
 
-            firebase.database().ref('Orders/' + vm.user).set({
-                username: name,
-                email: email,
-                profile_picture : imageUrl
-            });
         };
+
+
+
+        var orderRef = firebase.database().ref('orders/' + vm.user + '/');
+        orderRef.on('value', function(snapshot) {
+            snapshot.forEach(function (snap) {
+                console.log(snap.val());
+                console.log(snap.key);
+            })
+        });
+
 
 
     }
